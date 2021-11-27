18daf93d73260c60b57986468baf4347cf029022
lijie <lj19102837815@163.com>
2021-11-27 16:30:12 +0800

    首页模块完成，并且更新了静态资源文件夹。

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,35 +1,31 @@
 $(function() {
-    // 点击“去注册账号”的链接
-    $('#link_reg').on('click', function(e) {
-        e.preventDefault();
+    // 点击 去注册账号
+    $('#link_reg').on('click', function() {
         $('.login-box').hide()
         $('.reg-box').show()
     })
 
-    // 点击“去登录”的链接
-    $('#link_login').on('click', function(e) {
-        e.preventDefault();
+    // 点击 去登录
+    $('#link_login').on('click', function() {
         $('.login-box').show()
         $('.reg-box').hide()
     })
 
-    // 从 layui 中获取 form 对象
+    // 从 layui 中获取 form 对象。
     var form = layui.form
-        // 通过 form.verify() 函数自定义校验规则
+        // 通过 form.verify() 函数自定义校验规则。
     form.verify({
-        // 密码的校验规则
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格。'],
-        // 验证密码的校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repwd: function(value) {
-            // 形参 value 拿到的是确认密码框中的值
+            // 通过函数形参拿到的是确认密码框中的内容。
             var pwd = $('.reg-box [name=password]').val()
             if (pwd !== value) {
-                return '两次密码输入不一致！'
+                return '两次密码不一致！'
             }
         }
     })
 
-    // 使用 layer 提示消息
+    // 从 layui 中获取 layer 对象。
     var layer = layui.layer
         // 监听注册表单的提交事件
     $('#form_reg').on('submit', function(e) {
@@ -60,9 +56,8 @@ $(function() {
                     return layer.msg('登录失败！')
                 }
                 layer.msg('登录成功！')
-                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
-                sessionStorage.setItem('token', res.token)
-                    // 跳转到后台主页
+                    // 将登录成功得到的 token 字符串，保存到 localStorage 中。
+                localStorage.setItem('token', res.token)
                 location.href = '/index.html'
             }
         })
